7b56b661bbb62dcccdc0873c278043caa5a3ece1
Benjamin Anderson <benjaminghray@gmail.com>
2020-06-21 01:25:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,6 @@
 # Write your code here.
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def badge_maker(name)
-  badge_maker("Arel")
-  puts "Hello, my name is #{name}."
-end
-
+attendee = "Arel"
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
     "Hello, my name is #{attendee}."
